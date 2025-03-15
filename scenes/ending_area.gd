599e69ca4087b4ab0472bc8c777534298faf669f
c1foZ <tomife2@gmail.com>
2025-03-15 10:05:19 +0100

    make 3 levels and loop

---
+++ scenes/ending_area.gd
@@ -0,0 +1,15 @@
+extends Area2D
+
+const FILE_BEGIN = "res://scenes/levels/level_"
+
+func _on_body_entered(body: Node2D) -> void:
+	if body.is_in_group("Player"):
+		var current_scene_file = get_tree().current_scene.scene_file_path
+		var next_level_number = current_scene_file.to_int() + 1
+		
+		var next_level_path = FILE_BEGIN + str(next_level_number) + ".tscn"
+		
+		call_deferred("change_scene", next_level_path)
+
+func change_scene(next_level_path: String) -> void:
+	get_tree().change_scene_to_file(next_level_path)
