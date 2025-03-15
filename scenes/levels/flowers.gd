80f560694c919f83fa75c9b59113acd618e67b51
c1foZ <tomife2@gmail.com>
2025-03-16 00:42:28 +0100

    flower selection fixed

--- scenes/levels/flowers.gd
+++ scenes/levels/flowers.gd
@@ -12,13 +12,11 @@ func _on_flower_button_pressed() -> void:
 	flower_broken.queue_free()
 	
 	var tween = get_tree().create_tween()
-	flower_new.modulate.a = 0  # Start fully transparent
+	#flower_new.modulate.a = 0  # Start fully transparent
 	flower_new.visible = true
-	tween.tween_property(flower_new, "modulate:a", 1, 0.5)  # Fade in
-	
-	 # Highlight the button before disabling it
-	flower_button.modulate = Color(1, 1, 0)  # Change to yellow for highlight
-	await get_tree().create_timer(0.2).timeout  # Wait for highlight effect
+	#tween.tween_property(flower_new, "modulate:a", 1, 0.5)  # Fade in
+	flower_button.disabled = true
+	flower_button.queue_free()
 	
 	var body = get_tree().get_nodes_in_group("Player")[0]
 	var dialog_node = body.get_node("Dialog")
