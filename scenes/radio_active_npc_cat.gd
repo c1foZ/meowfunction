90e558560d3e914dfeb162f67c15c21468f73dbb
c1foZ <tomife2@gmail.com>
2025-03-15 16:59:05 +0100

    adding meow to radio active cat

--- scenes/radio_active_npc_cat.gd
+++ scenes/radio_active_npc_cat.gd
@@ -1,11 +1,16 @@
 extends CharacterBody2D
 
-@onready var bird_audio = $BirdSound
+@onready var cat_audio = $BirdSound
 @onready var animation_player = $AnimatedSprite2D
+@onready var meow_sound = $MeowSound
 
 func _process(_delta):
 	animation_player.play("idle")
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body.is_in_group("Player"):
-		bird_audio.play()
+		cat_audio.play()
+
+func _on_button_pressed() -> void:
+	cat_audio.stream = meow_sound.stream
+	cat_audio.play()
