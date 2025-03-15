47dd5fd8a32cc4d9a2fe6a2c6981e44ef050ac77
c1foZ <tomife2@gmail.com>
2025-03-15 13:37:53 +0100

    added tree interaction

---
+++ scenes/levels/tree.gd
@@ -0,0 +1,17 @@
+extends Node
+
+@export var old_tree: Node
+@export var new_tree: Node
+
+func _on_tree_button_pressed() -> void:
+	print("Tree button is pressed")
+
+	var tween = get_tree().create_tween()
+
+	if old_tree:
+		old_tree.visible = false  # Hide after fade-out
+
+	if new_tree:
+		new_tree.modulate.a = 0  # Start fully transparent
+		new_tree.visible = true
+		tween.tween_property(new_tree, "modulate:a", 1, 0.5)  # Fade in
