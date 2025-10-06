3092f3d9fbec631e375cffc0ff1d0bd8ca56212b
c1foZ <tomife2@gmail.com>
2025-10-06 07:18:29 +0200

    fix dialogs2

--- scripts/objectives/path.gd
+++ scripts/objectives/path.gd
@@ -6,22 +6,25 @@ extends Node
 
 signal objective_done
 
+var player_ref: Node2D
+var in_area := false
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body.is_in_group("Player2"):
+		in_area = true
+		player_ref = body
+		player_ref.can_move = false
 		var dialog_node = body.get_node("Dialog2")
 		dialog_node.visible = true
-		var rich_text = dialog_node.get_node("RichTextLabel")
-		var current_size = 16
-		rich_text.add_theme_font_size_override("normal_font_size", current_size - 2)
+		var rich_text = dialog_node.get_node("RichTextLabelBigger")
+		dialog_node.get_node("Sprite2D").visible = false
+		dialog_node.get_node("RichTextLabel").visible = false
+		dialog_node.get_node("Sprite2DBigger").visible = true
+		dialog_node.get_node("RichTextLabelBigger").visible = true
 		rich_text.text = "Fragments of a path, scattered and forgotten."
-		await get_tree().create_timer(2.0).timeout
-		dialog_node.visible = false
-		dialog_node.visible = true
+		await get_tree().create_timer(2).timeout
 		rich_text.text = "Could they ever fit together?"
-		await get_tree().create_timer(2.0).timeout
-		dialog_node.visible = false
-
+		set_process_input(true)
 
 func _on_path_button_pressed() -> void:
 	path_button.disabled = true
@@ -44,3 +47,10 @@ func _on_path_button_pressed() -> void:
 	rich_text.text = "...something feels off."
 	await get_tree().create_timer(2.0).timeout
 	dialog_node.visible = false
+
+func _input(event):
+	if in_area and event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
+		var dialog_node = player_ref.get_node("Dialog2")
+		dialog_node.visible = false
+		player_ref.can_move = true
+		set_process_input(false)
